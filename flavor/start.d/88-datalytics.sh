7a1b508c3d6482b96588f7b998c19ab65360c2ac
Matthias <m@tth.es>
2019-09-11 16:12:15 +0200

    set kick_yml to /opt

--- flavor/start.d/88-datalytics.sh
+++ flavor/start.d/88-datalytics.sh
@@ -6,4 +6,6 @@
 KICK_YML_FILE=/datalytics/.kick.yml sudo -E -s -u user kick write_config_file
 KICK_YML_FILE=/datalytics/.kick.yml sudo -E -s -u user kick init
 
+KICK_YML_FILE=/opt/.kick.yml
+
 service apache2 start
