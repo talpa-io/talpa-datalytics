01934235b03433ad531b2fcca6e3cc61eec83ad3
Matthias <m@tth.es>
2019-10-08 18:41:35 +0200

    added pgsql bindings

--- flavor/flavor-build.sh
+++ flavor/flavor-build.sh
@@ -8,7 +8,7 @@
 set -e
 
 apt-get update
-apt-get install -y apache2 libapache2-mod-php7.2 composer php7.2-curl php-xml php-zip php-mbstring php-yaml php-json php-http python3-venv
+apt-get install -y apache2 libapache2-mod-php7.2 composer php7.2-curl php-xml php-zip php-mbstring php-yaml php-json php-http python3-venv libpq-dev postgresql-common
 
 cp /datalytics/000-default.conf /etc/apache2/sites-available/000-default.conf
 
